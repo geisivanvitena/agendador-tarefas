83a7b05d410a4a49451ade12dc0097f351b33418
Geisivan <gsv1205@yahoo.com>
2026-01-03 21:44:18 -0300

    adicionadas classes exceptions

--- src/main/java/com/geisivan/agendadortarefas/infrastructure/repository/TarefaRepository.java
+++ src/main/java/com/geisivan/agendadortarefas/infrastructure/repository/TarefaRepository.java
@@ -1,6 +1,7 @@
 package com.geisivan.agendadortarefas.infrastructure.repository;
 
 import com.geisivan.agendadortarefas.infrastructure.entity.TarefaEntity;
+import com.geisivan.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,9 @@ import java.util.List;
 @Repository
 public interface TarefaRepository extends MongoRepository<TarefaEntity, String> {
 
-    List<TarefaEntity> findByDataEventoBetween(LocalDateTime dataInicio, LocalDateTime dataFim);
+    List<TarefaEntity> findByDataEventoBetweenAndStatusNotificacaoEnum(LocalDateTime dataInicio,
+                                                                       LocalDateTime dataFim,
+                                                                       StatusNotificacaoEnum status);
 
     List<TarefaEntity> findByEmailUsuario(String email);
 }
